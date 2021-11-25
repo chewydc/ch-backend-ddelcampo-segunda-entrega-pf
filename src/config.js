9b0827e1ab63b7d9b17506de55a68ca50294e159
chewydc <91146496+chewydc@users.noreply.github.com>
2021-11-25 12:02:08 -0300

    Hide Private Key
    I hide the private keys for accessing the DBs in config.js

--- src/config.js
+++ src/config.js
@@ -9,15 +9,24 @@ export default {
     },
     mongodb: {
         //url: 'mongodb://localhost:27017/ecommerce',
-        url: 'mongodb+srv://admin:admin@cluster0.e47it.mongodb.net/ecommerce',
+        url: 'mongodb+srv://xxx:xxx@cluster.xxx.mongodb.net/ecommerce',
         options: {
             serverSelectionTimeoutMS: 5000
         }
     },
     firebase: {
         serviceAccount: {
-            xxx
+            "type": "service_account",
+            "project_id": "basefirebase-xxx",
+            "private_key_id": "xxx",
+            "private_key": "-----BEGIN PRIVATE KEY-----\xxx\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-xxx",
+            "client_id": "XXX",
+            "auth_uri": "https://...h",
+            "token_uri": "https://...",
+            "auth_provider_x509_cert_url": "https://...",
+            "client_x509_cert_url": "https://..."
         },
-        url: "https://basefirebase-4f09d.firebaseio.com"
+        url: "https://basefirebase-xxx.firebaseio.com"
     }
 }
